559b799e0bdbd49532b13b67912ae09118a08173
Tomara Petty <tomararuth@gmail.com>
2021-03-10 12:25:28 -0800

    added testimonial box css and html

---
+++ script.js
@@ -0,0 +1,7 @@
+const footer = document.getElementById('footer');
+
+const currentDate = new Date().getFullYear();
+
+const footerYear = HTMLElement.innerText
+
+footer.innerText = "Copyright © " + currentDate;
