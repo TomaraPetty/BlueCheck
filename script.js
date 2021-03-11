14a2a9a419e970015404452b7544642543e27ae7
Tomara Petty <tomararuth@gmail.com>
2021-03-10 17:49:08 -0800

    updated styling and added founder section.

--- script.js
+++ script.js
@@ -14,23 +14,30 @@ const testimonials = [
       "Tomara rocks! Blue Check came through for us. She was easy to work with and got the job done in just a few days.",
   },
   {
-    name: "Jo Shmo",
-    position: "Marketing",
+    name: "Troy Ogletree",
+    position: "Pop artist",
     photo:
-      "https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg",
+      "https://pbs.twimg.com/profile_images/549991240415313920/BWEI-HJ4.jpeg",
     text:
-      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guys is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought our design and do theri best to put it in code.",
+      "Props to Tomara for making the verification process painless and fast - only taking a few days in my case! Being verified has helped me gain more validity as a musician and opened up the opportunity to collab with some amazing artists.",
   },
   {
     name: "Jessica Anderson",
     position: "Music artist",
     photo:
       "https://pbs.twimg.com/profile_images/964693148491329536/4tuJzx-j_400x400.jpg",
-    text: "i love them",
+    text: "i love Blue Check!",
+  },
+  {
+    name: "Everything Prehistoric",
+    position: "Dinosaur Museum",
+    photo:
+      "https://boneclones.com/images/store-product/product-1551-main-main-big-1415043687.jpg",
+    text: "We are a small business with a big reputation. Blue Check helped us secure verification for our museum which has made it easier for our customers to find us online. Tomara helped us out digitally so now we can focus on what we do best.",
   },
 ];
 
-let idx = 0;
+let idx = 1;
 
 function updateTestimonial() {
   const { name, position, photo, text } = testimonials[idx];
