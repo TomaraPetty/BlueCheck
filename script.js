a77fd5895ffc265f2fde071a13fab628e7e53c6c
Tomara Petty <tomararuth@gmail.com>
2021-03-14 12:37:13 -0700

    adding testimonial and updating verification description. Adding style dynamic style elements to the profile photo in mobile view

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ const footer = document.getElementById("footer");
 
 const testimonials = [
   {
-    name: "All My Friends Hate Me",
-    position: "Punk Band",
-    photo: "https://i.ido.bi/assets/post/2020/11/All-My-Friends-Hate-Me-650x650.jpg",
-    text:
-      "Tomara is a friend of the band. She got us verified for a birthday present, which was a killer surprise.",
-  },
-  {
     name: "Troy Ogletree",
     position: "Pop artist",
     photo:
       "https://pbs.twimg.com/profile_images/549991240415313920/BWEI-HJ4.jpeg",
     text:
       "Props to Tomara for making the verification process painless and fast - only taking a few days in my case! Being verified has helped me gain more validity as a musician and opened up the opportunity to collab with some amazing artists.",
+  },
+  {
+    name: "Diana Meyer",
+    position: "Rock musician",
+    photo:
+      "https://pbs.twimg.com/profile_images/1086022851503648769/baqPNKru.jpg",
+    text:
+      "Tomara is awesome! the process was easy, straightforward and only took a few days. I definitely recommend working with her.",
+  },
+  {
+    name: "All My Friends Hate Me",
+    position: "Punk Band",
+    photo: "https://i.ido.bi/assets/post/2020/11/All-My-Friends-Hate-Me-650x650.jpg",
+    text:
+      "Tomara is a friend of the band. She got us verified for a birthday present, which was a killer surprise.",
   },
   {
     name: "Everything Prehistoric",
